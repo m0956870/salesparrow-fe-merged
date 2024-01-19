55d4a6c24417e00deba57887bb1707c6a9e33807
VIvekTekniko <vivektekniko@gmail.com>
2024-01-19 18:20:01 +0530

    test 2

--- src/components/LeadManagementNew/Content/Banner/BannerListing.jsx
+++ src/components/LeadManagementNew/Content/Banner/BannerListing.jsx
@@ -6,9 +6,7 @@ import { useNavigate } from "react-router-dom";
 import CategoryBannerListing from "./CategoryBannerListing";
  
 const BannerListing = () => {
-
   const navigate = useNavigate();
-
   const [banner, setBanner] = useState([]);
   const [pageLength, setpageLength] = useState();
   const [catName , setCatName] =  useState()
